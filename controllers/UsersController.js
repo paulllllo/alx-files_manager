37459ff003ebd7cd2ec96a084852133e0fc1f657
kelechi <paulkelechi11@gmail.com>
2023-12-29 22:31:25 +0000

    Corrections

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,35 +6,35 @@ import dbClient from '../utils/db';
 const userQueue = new Queue('email sending');
 
 export default class UsersController {
-    static async postNew(req, res) {
+  static async postNew(req, res) {
     const email = req.body ? req.body.email : null;
     const password = req.body ? req.body.password : null;
 
     if (!email) {
-        res.status(400).json({ error: 'Missing email' });
-        return;
+      res.status(400).json({ error: 'Missing email' });
+      return;
     }
     if (!password) {
-        res.status(400).json({ error: 'Missing password' });
-        return;
+      res.status(400).json({ error: 'Missing password' });
+      return;
     }
     const user = await (await dbClient.usersCollection()).findOne({ email });
 
     if (user) {
-        res.status(400).json({ error: 'Already exist' });
-        return;
+      res.status(400).json({ error: 'Already exist' });
+      return;
     }
     const insertionInfo = await (await dbClient.usersCollection())
-        .insertOne({ email, password: sha1(password) });
+      .insertOne({ email, password: sha1(password) });
     const userId = insertionInfo.insertedId.toString();
 
     userQueue.add({ userId });
     res.status(201).json({ email, id: userId });
-    }
+  }
 
-    static async getMe(req, res) {
+  static async getMe(req, res) {
     const { user } = req;
 
     res.status(200).json({ email: user.email, id: user._id.toString() });
-    }
+  }
 }

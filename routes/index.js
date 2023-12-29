f87d04ab1a3a8d42676aaab86c2e9be6d26472b8
kelechi <paulkelechi11@gmail.com>
2023-12-29 19:20:37 +0000

    Corrections

--- routes/index.js
+++ routes/index.js
@@ -12,26 +12,26 @@ import { APIError, errorResponse } from '../middlewares/error';
  * @param {Express} api
  */
 const injectRoutes = (api) => {
-    api.get('/status', AppController.getStatus);
-    api.get('/stats', AppController.getStats);
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats);
 
-    api.get('/connect', basicAuthenticate, AuthController.getConnect);
-    api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+  api.get('/connect', basicAuthenticate, AuthController.getConnect);
+  api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
 
-    api.post('/users', UsersController.postNew);
-    api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+  api.post('/users', UsersController.postNew);
+  api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
 
-    api.post('/files', xTokenAuthenticate, FilesController.postUpload);
-    api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
-    api.get('/files', xTokenAuthenticate, FilesController.getIndex);
-    api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
-    api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
-    api.get('/files/:id/data', FilesController.getFile);
+  api.post('/files', xTokenAuthenticate, FilesController.postUpload);
+  api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
+  api.get('/files', xTokenAuthenticate, FilesController.getIndex);
+  api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
+  api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
+  api.get('/files/:id/data', FilesController.getFile);
 
-    api.all('*', (req, res, next) => {
+  api.all('*', (req, res, next) => {
     errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
-    });
-    api.use(errorResponse);
+  });
+  api.use(errorResponse);
 };
 
 export default injectRoutes;
